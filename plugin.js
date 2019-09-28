b2d80b16d7a3526c0266668a76b5f8c7075a7e11
Ed <ed3@users.noreply.github.com>
2019-09-28 11:15:51 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,8 +1,8 @@
-CKEDITOR.plugins.add('ckeditor_fa',{
+CKEDITOR.plugins.add('ckeditorfa',{
 icons:'ckeditor-fa',
 init:function(editor){
-editor.addCommand('ckeditor_fa', new CKEDITOR.dialogCommand('ckeditorFaDialog',{allowedContent:'span(!fa)'}));
-editor.ui.addButton('ckeditor_fa',{label:'FontAwesome icons',command:'ckeditor_fa',toolbar:'insert',icon:this.path + 'icons/ckeditor-fa.png'});
+editor.addCommand('ckeditorfa', new CKEDITOR.dialogCommand('ckeditorFaDialog',{allowedContent:'span(!fa)'}));
+editor.ui.addButton('ckeditorfa',{label:'FontAwesome icons',command:'ckeditorfa',toolbar:'insert',icon:this.path + 'icons/ckeditor-fa.png'});
 CKEDITOR.dialog.add('ckeditorFaDialog', this.path + 'dialogs/ckeditor-fa.js');
 CKEDITOR.document.appendStyleSheet(this.path + 'css/ckeditor-fa.css');
 }
