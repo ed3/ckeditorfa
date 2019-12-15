d0b9bfa3578f9f56c3001dabdffd3af6e8e1b617
Ed <ed3@users.noreply.github.com>
2019-12-15 13:01:14 +0200

    Add files via upload

--- plugin.js
+++ plugin.js
@@ -1,8 +1,8 @@
-CKEDITOR.plugins.add('ckeditorfa5',{
+CKEDITOR.plugins.add('ckeditorfa',{
 icons:'ckeditorfa',
 init:function(editor){
-editor.addCommand('ckeditorfa5', new CKEDITOR.dialogCommand('ckeditorFaDialog',{allowedContent:'span(!fa-*)'}));
-editor.ui.addButton('ckeditorfa5',{label:'FontAwesome5 icons',command:'ckeditorfa5',toolbar:'insert',icon:this.path + 'icons/ckeditorfa.png'});
+editor.addCommand('ckeditorfa', new CKEDITOR.dialogCommand('ckeditorFaDialog',{allowedContent:'span(!fa-*)'}));
+editor.ui.addButton('ckeditorfa',{label:'FontAwesome5 icons',command:'ckeditorfa',toolbar:'insert',icon:this.path + 'icons/ckeditorfa.png'});
 CKEDITOR.dialog.add('ckeditorFaDialog', this.path + 'dialogs/ckeditorfa.js');
 CKEDITOR.document.appendStyleSheet(this.path + 'css/ckeditorfa.css');
 }
