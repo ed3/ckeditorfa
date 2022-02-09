fc805077ef7fec7757dc7c3ce6f6ebff0344ab04
Ed <ed3@users.noreply.github.com>
2022-02-09 10:46:57 +0200

    fa6.0

--- plugin.js
+++ plugin.js
@@ -1,5 +1,4 @@
-CKEDITOR.plugins.add('ckeditorfa',{
-icons:'ckeditorfa',
+CKEDITOR.plugins.add('ckeditorfa',{icons:'ckeditorfa',
 init:function(editor){
 editor.addCommand('ckeditorfa', new CKEDITOR.dialogCommand('ckeditorFaDialog',{allowedContent:'span(!fa-*)'}));
 editor.ui.addButton('ckeditorfa',{label:'FontAwesome6 icons',command:'ckeditorfa',toolbar:'insert',icon:this.path + 'icons/ckeditorfa.png'});
